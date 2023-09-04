fbf03ebaf3dd51d7b60438f886b5795a15278aaf
Priyesh Mishra <priyeshbitsh@gmail.com>
2023-09-04 09:03:20 +0530

    Create LeetCode 141. Linked List Cycle (Daily September LeetCoding Challenge)

---
+++ Easy/141. Linked List Cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution 
+{
+    public:
+    bool hasCycle(ListNode *head) 
+    {
+        ListNode* hare = head;
+        ListNode* tortoise = head;
+        
+        while(hare && hare->next)
+        {
+            tortoise = tortoise->next;
+            hare = hare->next->next;
+
+            if(tortoise == hare) return true;
+        }
+        
+        return false;
+    }
+};
